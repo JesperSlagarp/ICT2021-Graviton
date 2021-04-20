8e143d46dd217a261b74ec7fd4f9912f70fa87df
JesperSlagarp <61503896+JesperSlagarp@users.noreply.github.com>
2021-04-20 09:30:27 +0200

    pathfinding

---
+++ Graviton/Assets/cameraMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+
+    public Transform player;
+
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
