44ed50df4865c89260c951164df3a4f4d7a4b064
JesperSlagarp <61503896+JesperSlagarp@users.noreply.github.com>
2021-04-22 08:07:26 +0200

    stuffs

--- Graviton/Assets/rangedTargetScript.cs
+++ Graviton/Assets/rangedTargetScript.cs
@@ -2,34 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class targetScript : MonoBehaviour
+public class rangedTargetScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform mob;
     public Transform player;
+    private Vector3 lastSeen;
     RaycastHit2D hit;
     void Start()
     {
-        
+        lastSeen = mob.position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (canSeePlayer())
+        if (CanSeePlayer())
         {
-            transform.position = player.position;
+            lastSeen = player.position;
+            this.transform.position = mob.position;
         } else {
-            
-            transform.position = mob.position;        
+            this.transform.position = lastSeen;
         }
     }
 
-    bool canSeePlayer() {
+    bool CanSeePlayer() {
         Vector2 v = new Vector2(player.position.x - mob.position.x, player.position.y - mob.position.y);
         float distance = v.magnitude;
         hit = Physics2D.Raycast(mob.position, v, distance, 0x1 << LayerMask.NameToLayer("obstacles"));
-        if (hit.collider != null) return true;
+        if (hit.collider == null) return true;
         else return false;
     }
 }
